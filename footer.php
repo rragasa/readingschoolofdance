773a06b2d95d24faeacb36eb24165bbd6a2ed120
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-10-05 17:59:25 +0100

    [TASK#10] - Adding contact us section

--- footer.php
+++ footer.php
@@ -1,6 +1,68 @@
-<section class="bg-black">
-    <div></div>
-</section>
+        <section class="bg-black contactus">
+            <div class="container">
+                <div class="contactus-item">
+                    <i class="fas fa-mobile-alt"></i>
+                    <div class="contactus-info">
+                        <h6>PHONE</h6>
+                        <p>+44 07856870559</p>
+                    </div>
+                </div>
+                <div class="contactus-item">
+                    <i class="fas fa-envelope"></i>
+                    <div class="contactus-info">
+                        <h6>EMAIL</h6>
+                        <p>Josie.norman@readingschoolofdance.com</p>
+                    </div>
+                </div>
+                <div class="contactus-item">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="contactus-info">
+                        <h6>Denefield School</h6>
+                        <p>
+                            Long Lane,
+                            <br>Tilehurst,
+                            <br>RG31 6XY
+                        </p>
+                    </div>
+                </div>
+                <div class="contactus-item">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="contactus-info">
+                        <h6>Caversham Heights Methodist Church</h6>
+                        <p>
+                            74 Highmoor Road,
+                            <br>Reading,
+                            <br>RG4 7BG
+                        </p>
+                    </div>
+                </div>
+                <div class="contactus-item">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="contactus-info">
+                        <h6>Highdown School</h6>
+                        <p>
+                            Surley Row,
+                            <br>Emmer Green,
+                            <br>Reading,
+                            <br>RG4 8LR
+
+                        </p>
+                    </div>
+                </div>
+                <div class="contactus-item">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="contactus-info">
+                        <h6>St Barnabas Hall</h6>
+                        <p>
+                            St Barnabas Road,
+                            <br>Emmer Green,
+                            <br>Reading,
+                            <br>RG4 8RA
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!-- Footer -->
         <footer class="rsd-footer bg-black text-light-grey">
             <div class="container col-xs-12 col-sm-3 col-md-3 col-lg-3">
@@ -12,7 +74,7 @@
                 </div>
             </div>
             <div class="container col-xs-12 col-sm-8 col-md-8 col-md-8">
-                Copyright &copy; Reading School of Dance 2018</p>
+                Copyright &copy; Reading School of Dance 2018
             </div>
         </footer>
 

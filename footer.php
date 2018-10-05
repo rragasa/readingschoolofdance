87fb263250993af4494f687146c540fb23929565
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-10-05 14:43:14 +0100

    [TASK#7] - fixing bug with footer when toggle

--- footer.php
+++ footer.php
@@ -16,6 +16,9 @@
             <div class="container">Copyright &copy; Reading School of Dance 2018</div>
         </footer>
 
+        </div>
+        <!-- /#wrapper -->
+
         <!-- Vendor Scripts -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" crossorigin="anonymous"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" crossorigin="anonymous"></script>

735ac7271f2e2b7a7b5c4933f38e081314471b45
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-10-22 16:48:14 +0100

    [commit#3] - New header and nav

--- header.php
+++ header.php
@@ -6,23 +6,33 @@
     <title>Reading School of Dance</title>
 
     <!-- VENDOR CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- CUSTOM CSS -->
     <link href="<?php echo get_template_directory_uri() . '/style.css'; ?>" rel="stylesheet">
 </head>
 
 <body>
-    <div class="menu-section">
-        <div class="menu-toggle">
-            <div class="one"></div>
-            <div class="two"></div>
-            <div class="three"></div>
+    <header class="hero">
+        <img class="alpha-bg" src="http://localhost:8888/wp-content/uploads/2018/10/756C0258-1.jpg">
+
+        <div class="menu-section">
+            <div class="menu-toggle">
+                <div class="one"></div>
+                <div class="two"></div>
+                <div class="three"></div>
+            </div>
+            <nav>
+                <ul role="navigation" class="hidden">
+                <?php
+                        wp_nav_menu(array('menu' => 'primary', 'items_wrap' => '<li id="item-id"></li>%3$s'));
+                    ?>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul role="navigation" class="hidden">
-            <?php
-                    wp_nav_menu(array('menu' => 'primary', 'items_wrap' => '<li id="item-id"></li>%3$s'));
-                ?>
-            </ul>
-        </nav>
-    </div>
+
+
+        <div class="wrapper ml4">
+            <h1 class="supersize montserrat">Reading School of <span class="color-emphasis">Dance.</span></h1>
+        </div>
+    </header>

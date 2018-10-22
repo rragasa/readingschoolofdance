c6281cc9a68251db23084360a32e7af9061ac7f8
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-10-22 13:21:07 +0100

    [commit#1] - Starting again

--- header.php
+++ header.php
@@ -6,56 +6,9 @@
     <title>Reading School of Dance</title>
 
     <!-- VENDOR CSS -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
     <!-- CUSTOM CSS -->
     <link href="<?php echo get_template_directory_uri() . '/style.css'; ?>" rel="stylesheet">
 </head>
 
 <body>
-    <div id="wrapper">
-        <div class="overlay"></div>
-
-        <!-- Sidebar -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
-        <?php wp_nav_menu( array('menu' => 'primary','container'=> '','items_wrap'=>'<ul class="nav sidebar-nav"><li class="sidebar-brand"><a href="#">Menu</a></li>%3$s</ul>')); ?>
-        </nav>
-        <!-- /#sidebar-wrapper -->
-
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-          <button type="button" class="hamburger is-closed animated fadeInLeft" data-toggle="offcanvas">
-            <span class="hamb-top"></span>
-            <span class="hamb-middle"></span>
-            <span class="hamb-bottom"></span>
-          </button>
-          <!-- Header -->
-          <?php if (is_page(home)) { ?>
-            <header id="home">
-                <div class="header-img">
-                    <img src="<?php header_image();?>" alt="" />
-                </div>
-                <div class="header-text">
-                        <div class="container">
-                            <h1><?php the_field('header_title');?></h1>
-                            <div class="divider"></div>
-                            <p><?php the_field('header_description');?></p>
-                        </div>
-                    </div>
-            </header>
-        <?php } else { ?>
-                <header>
-                <div class="header-img">
-                    <img src="<?php header_image();?>" alt="" />
-                </div>
-                <div class="header-text">
-                    <div class="container">
-                        <h1><?php the_field('header_title');?></h1>
-                        <div class="divider"></div>
-                        <p><?php the_field('header_description');?></p>
-                    </div>
-                </div>
-            </header>
-        <?php }?>
--- index.php
+++ index.php
@@ -10,39 +10,7 @@
  *
  */
 get_header();?>
-        <section class="aboutus-section">
-            <div class="section-left col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-blue">
-                <img src="<?php the_field('aboutus-image');?>">
-            </div>
-            <div class="section-right container col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-black">
-                <div class="section-description">
-                    <h3 class="section-text"><a href="/about-us">About Us</a></h3>
-                </div>
-            </div>
-        </section>
 
-         <section class="classes-section">
-         <div class="section-left col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-blue">
-                <img src="<?php the_field('classes-image');?>">
-            </div>
-            <div class="section-right container col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-black">
-                <div class="section-description">
-                    <h3 class="section-text"><a href="/classes">Classes</a></h3>
-                </div>
-            </div>
-        </section>
 
-        <section class="gallery-section">
-            <div class="section-left col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-orange">
-                <img src="<?php the_field('gallery-image');?>">
-            </div>
-            <div class="section-right container col-xs-12 col-sm-6 col-md-6 col-lg-6 bg-black">
-                <div class="section-description">
-                    <h3 class="section-text"><a href="/gallery">Gallery</a></h3>
-                </div>
-            </div>
-        </section>
-    </div>
-    <!-- /#page-content-wrapper -->
 
 <?php get_footer();?>

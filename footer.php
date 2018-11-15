8022c7be8923e9a83801328142fa8a0cdca0e9cc
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-11-15 18:07:19 +0000

    [RSOD-1] - Removing all files and starting from scratch

--- footer.php
+++
@@ -1,7 +0,0 @@
-        <!-- Vendor Scripts -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-        <!-- Custom Scripts -->
-        <script type="text/javascript" src="<?php echo get_template_directory_uri() . '/assets/js/header.js'; ?>"></script>
-    </body>
-</html>
